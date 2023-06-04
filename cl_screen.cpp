ea013298d53b6260762cdfd0a0ad154ee39b87a5
deropleat <42844469+Deropleat@users.noreply.github.com>
2023-06-04 07:34:43 +0300

    Update cl_screen.cpp
    написал комментарии, объясняющие, для чего нужен тот или иной метод, и.т.п.

--- cl_screen.cpp
+++ cl_screen.cpp
@@ -1,11 +1,10 @@
 #include "cl_screen.h"
 
-cl_screen::cl_screen(cl_base* p_head_object) : cl_base(p_head_object, "cl_screen")
+cl_screen::cl_screen(cl_base* p_head_object) : cl_base(p_head_object, "cl_screen") // параметризированный конструктор объекта класса cl_screen
 {
-	
 }
 
-void cl_screen::print_message(std::string& message)
+void cl_screen::print_message(std::string& message) // метод вывода сообщения
 {
 	if (!message.empty()) std::cout << std::endl << message;
 }
