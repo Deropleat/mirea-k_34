e2eb6005aebe8f0de0f453f39318a805e7d031a6
deropleat <42844469+Deropleat@users.noreply.github.com>
2023-06-04 07:31:15 +0300

    Update cl_car.cpp
    Убрал деструктор объекта класса cl_car; сделал комментарии, объясняющие, для чего нужен тот или иной метод, и.т.п.

--- cl_car.cpp
+++ cl_car.cpp
@@ -1,15 +1,11 @@
 #include "cl_car.h"
 
-cl_car::cl_car(cl_base* p_head_object, std::string s_object_name) : cl_base(p_head_object, s_object_name)
+cl_car::cl_car(cl_base* p_head_object, std::string s_object_name) : cl_base(p_head_object, s_object_name) // параметризированный конструктор объекта класса cl_car
 {
 	number = s_object_name;
 }
 
-cl_car::~cl_car()
-{
-}
-
-std::string cl_car::get_number()
+std::string cl_car::get_number() // получение номера текущей машины
 {
 	return number;
 }
