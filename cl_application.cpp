81bd9462c9ff07ff16bcd904f204edf866e87c8a
deropleat <42844469+Deropleat@users.noreply.github.com>
2023-06-04 07:28:57 +0300

    Update cl_application.cpp
    Сделал комментарии, объясняющие для чего нужен тот или иной метод и.т.п.

--- cl_application.cpp
+++ cl_application.cpp
@@ -1,13 +1,13 @@
 #include "cl_application.h"
 
-cl_application::cl_application(cl_base* p_head_object) : cl_base(p_head_object, "cl_application")
+cl_application::cl_application(cl_base* p_head_object) : cl_base(p_head_object, "cl_application") // параметризированный конструктор объекта класса cl_application
 {
 	controller = new cl_controller(this);
 	input = new cl_input(this);
 	screen = new cl_screen(this);
 }
 
-void cl_application::build_tree_objects()
+void cl_application::build_tree_objects() // ввод исходных данных о заправочной станции
 {
 	set_state_all(1);
 
@@ -22,7 +22,7 @@ void cl_application::build_tree_objects()
 	delete_object_connection(SIGNALFN(cl_input::readline), controller, HANDLERFN(cl_controller::gasoline_handler));
 }
 
-int cl_application::exec_app()
+int cl_application::exec_app() // ввод и отработка коменд для отработки заправочной станции
 {
 	std::string message;
 	setup_object_connection(SIGNALFN(cl_input::readline), controller, HANDLERFN(cl_controller::commands_handler));
@@ -42,13 +42,13 @@ int cl_application::exec_app()
 	std::cout << "Ready to work";
 	for (size_t i = 0; i < callback.size(); i++) send_object_signal(SIGNALFN(cl_application::get_callback), message);
 	delete_object_connection(SIGNALFN(cl_application::get_callback), screen, HANDLERFN(cl_screen::print_message));
-
+	
 	return 0;
 }
 
-void cl_application::get_callback(std::string& message)
+void cl_application::get_callback(std::string& message) // сигнал возврата полученной строки при выполнении команд
 {
-	if (callback.at(index) == "SHOWTREE") 
+	if (callback.at(index) == "SHOWTREE")
 	{
 		show_object_tree();
 		message = "";
