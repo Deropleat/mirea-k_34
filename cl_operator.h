bd697ef48ffea1468794b64791326cdf8828cba3
deropleat <42844469+Deropleat@users.noreply.github.com>
2023-06-04 07:28:06 +0300

    Update cl_operator.h
    Убрал объявление лишних методов и свойств

--- cl_operator.h
+++ cl_operator.h
@@ -7,15 +7,10 @@ class cl_operator : public cl_base
 {
 public:
 	cl_operator(cl_base*);
-
-	int get_served_cars();
+	
 	int can_service(std::vector<cl_gas_station*>, std::string, int);
 	size_t get_gas_station_by_name(std::vector<cl_gas_station*>, std::string);
 	std::string get_gas_stations_status(std::vector<cl_gas_station*>);
-
-	void add_served_counter(std::string&);
-private:
-	int served = 0;
 };
 
 #endif
