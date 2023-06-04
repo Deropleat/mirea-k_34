4b743990d82ebf00daa88f73ff7395320acdbc0a
deropleat <42844469+Deropleat@users.noreply.github.com>
2023-06-04 07:33:15 +0300

    Update cl_input.cpp
    Убрал базовый конструктор класса; написал комментарии, объясняющие, для чего нужен тот или иной метод, и.т.п.

--- cl_input.cpp
+++ cl_input.cpp
@@ -1,14 +1,10 @@
 #include "cl_input.h"
 
-cl_input::cl_input() : cl_base(nullptr, "") 
+cl_input::cl_input(cl_base* p_head_object) : cl_base(p_head_object, "cl_input") // параметризированный конструктор объекта класса cl_input
 {
 }
 
-cl_input::cl_input(cl_base* p_head_object) : cl_base(p_head_object, "cl_input") 
-{
-}
-
-void cl_input::readline(std::string& message)
+void cl_input::readline(std::string& message) // метод чтения введенной строки
 {
 	std::getline(std::cin, message);
 }
